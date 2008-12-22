f725f5a086000fe56c6cc41dc6a3d727fa3b31f0
أحمد محمد زواوي - Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2008-12-22 21:19:44 +0000

    Import of AZAWAWI/Padre-Plugin-Perl6-0.012 from CPAN.
    gitpan-cpan-distribution: Padre-Plugin-Perl6
    gitpan-cpan-version:      0.012
    gitpan-cpan-path:         AZAWAWI/Padre-Plugin-Perl6-0.012.tar.gz
    gitpan-cpan-author:       AZAWAWI
    gitpan-cpan-maturity:     released

--- lib/Padre/Document/Perl6.pm
+++ lib/Padre/Document/Perl6.pm
@@ -13,101 +13,181 @@ use Syntax::Highlight::Perl6;
 our $VERSION = '0.22';
 our @ISA     = 'Padre::Document';
 
-my $keywords;
+sub text_with_one_nl {
+    my $self = shift;
+    my $text = $self->text_get;
+    my $nlchar = "\n";
+    if ( $self->get_newline_type eq 'WIN' ) {
+        $nlchar = "\r\n";
+    }
+    elsif ( $self->get_newline_type eq 'MAC' ) {
+        $nlchar = "\r";
+    }
+    $text =~ s/$nlchar/\n/g;
+    return $text;
+}
 
 # Naive way to parse and colorize perl6 files
 sub colorize {
-	my ($self, $first) = @_;
+    my ($self, $first) = @_;
 
-	my $editor = $self->editor;
-	my $text   = $self->text_get;
-  
-  my $t0 = Benchmark->new;
-  my $p = Syntax::Highlight::Perl6->new(
-    text => $text,
-  );
-  
-  my @tokens;
+    my $editor = $self->editor;
+    my $text   = $self->text_with_one_nl;
 
-	eval {
-	@tokens = $p->tokens;
-  1;
-	};
-	
-	if($EVAL_ERROR) {
-		say "Parsing error, bye bye ->colorize";
-		return;
-	}
-	
-	$self->remove_color;
-	
-  my %colors = (
-		'comp_unit'  => Px::PADRE_BLUE, 
-		'scope_declarator' => Px::PADRE_RED,
-		'routine_declarator' => Px::PADRE_RED,
-		'regex_declarator' => Px::PADRE_RED,
-		'package_declarator' => Px::PADRE_RED,
-		'statement_control' => Px::PADRE_RED,
-		'block' => Px::PADRE_BLACK,
-		'regex_block' => Px::PADRE_BLACK,
-		'noun' => Px::PADRE_BLACK,
-		'sigil' => Px::PADRE_GREEN,
-		'variable' => Px::PADRE_GREEN, 
-		'assertion' => Px::PADRE_GREEN,
-		'quote' => Px::PADRE_MAGENTA,
-		'number' => Px::PADRE_ORANGE,
-		'infix' => Px::PADRE_DIM_GRAY,
-		'methodop' => Px::PADRE_BLACK,
-		'pod_comment' => Px::PADRE_GREEN,
-		'param_var' => Px::PADRE_CRIMSON,
-		'_scalar' => Px::PADRE_RED,
-		'_array' => Px::PADRE_BROWN,
-		'_hash' => Px::PADRE_ORANGE,
-		'_comment' => Px::PADRE_GREEN,
-  );
-  for my $htoken (@tokens) {
-    my %token = %{$htoken};
-    my $color = $colors{ $token{rule} };
-    if($color) {
-      my $len = length $token{buffer};
-      my $start = $token{last_pos} - $len;
-      $editor->StartStyling($start, $color);
-      $editor->SetStyling($len, $color);
+    my $t0 = Benchmark->new;
+    my $p = Syntax::Highlight::Perl6->new(
+        text => $text,
+    );
+
+    my @tokens;
+    eval { @tokens = $p->tokens;   1; };
+    $self->{issues} = [];
+    if($EVAL_ERROR) {
+        say "Parsing error, bye bye ->colorize " . $EVAL_ERROR;
+        my @errors = split /\n/, $EVAL_ERROR;
+        my $lineno = undef;
+        for my $error (@errors) {
+            if($error =~ /line (\d+):$/) {
+                $lineno = $1;
+            }
+            if($lineno) {
+                push @{$self->{issues}}, { line => $lineno, msg => $error, severity => 'E', };
+            }
+        }
+        return;
+    } 
+
+    $self->remove_color;
+
+    my %colors = (
+        'comp_unit'  => Px::PADRE_BLUE,
+        'scope_declarator' => Px::PADRE_RED,
+        'routine_declarator' => Px::PADRE_RED,
+        'regex_declarator' => Px::PADRE_RED,
+        'package_declarator' => Px::PADRE_RED,
+        'statement_control' => Px::PADRE_RED,
+        'block' => Px::PADRE_BLACK,
+        'regex_block' => Px::PADRE_BLACK,
+        'noun' => Px::PADRE_BLACK,
+        'sigil' => Px::PADRE_GREEN,
+        'variable' => Px::PADRE_GREEN,
+        'assertion' => Px::PADRE_GREEN,
+        'quote' => Px::PADRE_MAGENTA,
+        'number' => Px::PADRE_ORANGE,
+        'infix' => Px::PADRE_DIM_GRAY,
+        'methodop' => Px::PADRE_BLACK,
+        'pod_comment' => Px::PADRE_GREEN,
+        'param_var' => Px::PADRE_CRIMSON,
+        '_scalar' => Px::PADRE_RED,
+        '_array' => Px::PADRE_BROWN,
+        '_hash' => Px::PADRE_ORANGE,
+        '_comment' => Px::PADRE_GREEN,
+    );
+
+    for my $htoken (@tokens) {
+        my %token = %{$htoken};
+        my $color = $colors{ $token{rule} };
+        if($color) {
+              my $len = length $token{buffer};
+              my $start = $token{last_pos} - $len;
+              $editor->StartStyling($start, $color);
+              $editor->SetStyling($len, $color);
+        }
     }
-  }
-  
-  my $td = timediff(new Benchmark, $t0);
-  say "->colorize took:" . timestr($td) ;  
+
+      my $td = timediff(new Benchmark, $t0);
+      say "->colorize took:" . timestr($td) ;
 }
 
 sub get_command {
-	my $self     = shift;
-	
-	my $filename = $self->filename;
+    my $self     = shift;
 
-	if (not $ENV{PARROT_PATH}) {
-		die "PARROT_PATH is not defined. Need to point to trunk of Parrot SVN checkout.\n";
-	}
-	my $parrot = File::Spec->catfile($ENV{PARROT_PATH}, 'parrot');
-	if (not -x $parrot) {
-		die "$parrot is not an executable.\n";
-	}
-	my $rakudo = File::Spec->catfile($ENV{PARROT_PATH}, 'languages', 'perl6', 'perl6.pbc');
-	if (not -e $rakudo) {
-		die "Cannot find Rakudo ($rakudo)\n";
-	}
+    my $filename = $self->filename;
 
-	return qq{"$parrot" "$rakudo" "$filename"};
+    if (not $ENV{PARROT_PATH}) {
+        die "PARROT_PATH is not defined. Need to point to trunk of Parrot SVN checkout.\n";
+    }
+    my $parrot = File::Spec->catfile($ENV{PARROT_PATH}, 'parrot');
+    if (not -x $parrot) {
+        die "$parrot is not an executable.\n";
+    }
+    my $rakudo = File::Spec->catfile($ENV{PARROT_PATH}, 'languages', 'perl6', 'perl6.pbc');
+    if (not -e $rakudo) {
+        die "Cannot find Rakudo ($rakudo)\n";
+    }
+
+    return qq{"$parrot" "$rakudo" "$filename"};
 
 }
 
+# Checks the syntax of a Perl document.
+# Documented in Padre::Document!
+# Implemented as a task. See Padre::Task::SyntaxChecker::Perl6
+sub check_syntax {
+    my $self  = shift;
+    my %args  = @ARG;
+    $args{background} = 0;
+    return $self->_check_syntax_internals(\%args);
+}
+
+sub check_syntax_in_background {
+    my $self  = shift;
+    my %args  = @ARG;
+    $args{background} = 1;
+    return $self->_check_syntax_internals(\%args);
+}
+
+sub _check_syntax_internals {
+    my $self = shift;
+    my $args  = shift;
+
+    my $text = $self->text_with_one_nl;
+    unless ( defined $text and $text ne '' ) {
+        return [];
+    }
+
+    # Do we really need an update?
+    require Digest::MD5;
+    use Encode qw(encode_utf8);
+    my $md5 = Digest::MD5::md5(encode_utf8($text));
+    unless ( $args->{force} ) {
+        if ( defined( $self->{last_checked_md5} )
+             && $self->{last_checked_md5} eq $md5
+        ) {
+            return;
+        }
+    }
+    $self->{last_checked_md5} = $md5;
+
+    require Padre::Task::SyntaxChecker::Perl6;
+    my $task = Padre::Task::SyntaxChecker::Perl6->new(
+        notebook_page => $self->editor,
+        text => $text,
+        issues => $self->{issues},
+        ( exists $args->{on_finish} ? (on_finish => $args->{on_finish}) : () ),
+    );
+    if ($args->{background}) {
+        # asynchroneous execution (see on_finish hook)
+        $task->schedule();
+        return();
+    }
+    else {
+        # serial execution, returning the result
+        return() if $task->prepare() =~ /^break$/;
+        $task->run();
+        return $task->{syntax_check};
+    }
+    return;
+}
+
 sub keywords {
-	if (! defined $keywords) {
-		$keywords = YAML::Tiny::LoadFile(
-			Padre::Util::sharefile( 'languages', 'perl6', 'perl6.yml' )
-		);
-	}
-	return $keywords;
+    my $self = shift;
+    if (! defined $self->{keywords}) {
+        $self->{keywords} = YAML::Tiny::LoadFile(
+            Padre::Util::sharefile( 'languages', 'perl6', 'perl6.yml' )
+        );
+    }
+    return $self->{keywords};
 }
 
 sub comment_lines_str { return '#' }
--- lib/Padre/Plugin/Perl6.pm
+++ lib/Padre/Plugin/Perl6.pm
@@ -13,7 +13,7 @@ use feature qw(say switch);
 use IO::File;
 use File::Temp;
 
-our $VERSION = '0.011';
+our $VERSION = '0.012';
 
 use URI::file;
 use Syntax::Highlight::Perl6;
@@ -27,84 +27,105 @@ Readonly my $SIMPLE_HTML  => 'simple_html';
 Readonly my $SNIPPET_HTML => 'snippet_html';
 
 sub padre_interfaces {
-	'Padre::Plugin'         => 0.20,
+    return 'Padre::Plugin'         => 0.20,
 }
 
 
 sub menu_plugins_simple {
-	my $self = shift;
-	'Perl 6' => [
-		'Export Full HTML' => sub { $self->export_html($FULL_HTML); },
-		'Export Simple HTML' => sub { $self->export_html($SIMPLE_HTML); },
-		'Export Snippet HTML' => sub { $self->export_html($SNIPPET_HTML); },
-		'About' => sub { $self->show_about },
-	];
+    my $self = shift;
+    return 'Perl 6' => [
+        'Export Full HTML' => sub { $self->export_html($FULL_HTML); },
+        'Export Simple HTML' => sub { $self->export_html($SIMPLE_HTML); },
+        'Export Snippet HTML' => sub { $self->export_html($SNIPPET_HTML); },
+        'About' => sub { $self->show_about },
+    ];
 }
 
 sub registered_documents {
-	'application/x-perl6'    => 'Padre::Document::Perl6',
+    return 'application/x-perl6'    => 'Padre::Document::Perl6',
 }
 
 
 sub show_about {
-	my ($main) = @ARG;
+    my ($main) = @ARG;
 
-	my $about = Wx::AboutDialogInfo->new;
-	$about->SetName("Padre::Plugin::Perl6");
-	$about->SetDescription(
-		"Perl6 syntax highlighting\nthat is based on Syntax::Highlight::Perl6\n"
-	);
-	Wx::AboutBox( $about );
-	return;
+    my $about = Wx::AboutDialogInfo->new;
+    $about->SetName("Padre::Plugin::Perl6");
+    $about->SetDescription(
+        "Perl6 syntax highlighting that is based on\nSyntax::Highlight::Perl6\n"
+    );
+    Wx::AboutBox( $about );
+    return;
+}
+
+sub text_with_one_nl {
+    my $self = shift;
+    my $doc = shift;
+    my $text = $doc->text_get // '';
+    
+    my $nlchar = "\n";
+    if ( $doc->get_newline_type eq 'WIN' ) {
+        $nlchar = "\r\n";
+    }
+    elsif ( $doc->get_newline_type eq 'MAC' ) {
+        $nlchar = "\r";
+    }
+    $text =~ s/$nlchar/\n/g;
+    return $text;
 }
 
 sub export_html {
-	my ($self, $type) = @ARG;
+    my ($self, $type) = @ARG;
 
-	if(!defined Padre::Documents->current) {
-		return;
-	}
-
-	my $text = Padre::Documents->current->text_get() // '';
-
+    my $doc = Padre::Documents->current;
+    if(!defined $doc) {
+        return;
+    }
+    if($doc->get_mimetype ne q{application/x-perl6}) {
+        return;
+    }
+    
+    my $text = $self->text_with_one_nl($doc);
     my $p = Syntax::Highlight::Perl6->new(
         text => $text,
-        inline_resources => 1, 
+        inline_resources => 1,
     );
 
     my $html;
     eval {
-	    given($type) {
-			when ($FULL_HTML) { $html = $p->full_html; }
-			when ($SIMPLE_HTML) { $html = $p->simple_html; }
-			when ($SNIPPET_HTML) { $html = $p->snippet_html; }
-			default {
-				croak "'$type' should full_html, simple_html or snippet_html";
-			}
-		}
-		1;
-	};
+        given($type) {
+            when ($FULL_HTML) { $html = $p->full_html; }
+            when ($SIMPLE_HTML) { $html = $p->simple_html; }
+            when ($SNIPPET_HTML) { $html = $p->snippet_html; }
+            default {
+                croak "'$type' should full_html, simple_html or snippet_html";
+            }
+        }
+        1;
+    };
 
-	if($EVAL_ERROR) {
-		say 'Parsing error, bye bye ->export_html';
-		return;
-	}
+    if($EVAL_ERROR) {
+        say 'Parsing error, bye bye ->export_html';
+        return;
+    }
 
-	# create a temporary HTML file
-	my $tmp = File::Temp->new(SUFFIX => '.html');
-	$tmp->unlink_on_destroy(0);
-	my $filename = $tmp->filename;
-	print $tmp $html;
-	close $tmp
-		or croak "Could not close $filename";
+    # create a temporary HTML file
+    my $tmp = File::Temp->new(SUFFIX => '.html');
+    $tmp->unlink_on_destroy(0);
+    my $filename = $tmp->filename;
+    print $tmp $html;
+    close $tmp
+        or croak "Could not close $filename";
 
-	# try to open the HTML file
-	my $main   = Padre->ide->wx->main_window;
-	$main->setup_editor($filename);
+    # try to open the HTML file
+    my $main   = Padre->ide->wx->main_window;
+    $main->setup_editor($filename);
 
-	# launch the HTML file in your default browser
-	my $file_url = URI::file->new($filename);
-	Wx::LaunchDefaultBrowser($file_url);	
+    # launch the HTML file in your default browser
+    my $file_url = URI::file->new($filename);
+    Wx::LaunchDefaultBrowser($file_url);
+
+    return;
 }
 
 
